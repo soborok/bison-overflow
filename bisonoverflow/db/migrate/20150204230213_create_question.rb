312a0b7302e78d90a35c413288cc9b28b5414363
Philipp Blume & Shea Strausman <pair+pgblu.sstrausman@devbootcamp.com>
2015-02-05 13:13:19 -0600

    finished the functionality to add a question.

---
+++ bisonoverflow/db/migrate/20150204230213_create_question.rb
@@ -0,0 +1,10 @@
+class CreateQuestion < ActiveRecord::Migration
+  def change
+    create_table :questions do |t|
+      t.string :content
+      t.string :title
+
+      t.timestamps
+    end
+  end
+end
