71d8a2ab37dca6217dae548cc76d84f37da39192
Heejin Park <soborok@gmail.com>
2015-02-05 23:20:35 -0600

    created model, migration for User

---
+++ app/models/user.rb
@@ -0,0 +1,24 @@
+class User < ActiveRecord::Base
+
+	has_many :questions
+
+	validates_uniqueness_of :email
+
+	def password
+		BCrypt::Password.new(self.hashed_password)
+	end
+
+	def password=(user_input)
+		self.hashed_password = BCrypt::Password.create(user_input)
+	end
+
+	def self.authenticate(email, password)
+		user = User.find_by(email: email)
+		user if user && user.password == password
+	end
+
+	def full_name
+    "#{first_name} #{last_name}"
+  end
+
+end
