d90a22e7e4ed0df45e287e7a048269bb5c449b52
Shea Strausman <sstrausman@gmail.com>
2015-02-06 15:23:41 -0600

    fixed some small routes issues

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -3,16 +3,15 @@ class SessionsController < ActionController::Base
   def login
     if @user = User.authenticate(user_params[:email], user_params[:password])
       session[:user_id] = @user.id
-      redirect_to users_path
     else
       flash[:message] = "Please try again."
-      redirect_to login_path
     end
+    redirect_to root_path
   end
 
   def logout
     session.delete(:user_id)
-    redirect_to login_path
+    redirect_to root_path
   end
 
 
