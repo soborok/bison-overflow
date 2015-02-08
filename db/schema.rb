6a505eb2bf43988cdd7f8235e5a6320a3fb288e3
Shea Strausman <sstrausman@gmail.com>
2015-02-08 14:42:28 -0600

    added functionality to add tag. Generated models tag and tagging, changed question model and form as well as several views to accomplish this

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20150206172643) do
+ActiveRecord::Schema.define(version: 20150208200624) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -32,6 +32,20 @@ ActiveRecord::Schema.define(version: 20150206172643) do
     t.datetime "updated_at"
   end
 
+  create_table "taggings", force: :cascade do |t|
+    t.integer  "question_id"
+    t.integer  "tag_id"
+    t.datetime "created_at",  null: false
+    t.datetime "updated_at",  null: false
+  end
+
+  add_index "taggings", ["question_id"], name: "index_taggings_on_question_id", using: :btree
+  add_index "taggings", ["tag_id"], name: "index_taggings_on_tag_id", using: :btree
+
+  create_table "tags", force: :cascade do |t|
+    t.string "name"
+  end
+
   create_table "users", force: :cascade do |t|
     t.string   "email"
     t.string   "first_name"
@@ -41,4 +55,6 @@ ActiveRecord::Schema.define(version: 20150206172643) do
     t.datetime "updated_at"
   end
 
+  add_foreign_key "taggings", "questions"
+  add_foreign_key "taggings", "tags"
 end
